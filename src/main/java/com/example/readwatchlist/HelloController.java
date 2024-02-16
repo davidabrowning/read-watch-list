38ab8163739d512d90a15ba1dcc9e6ef20a1f7fe
David <11443294+davidabrowning@users.noreply.github.com>
2024-02-16 13:34:42 -0500

    Removed unused import statements from HelloController.java.

--- src/main/java/com/example/readwatchlist/HelloController.java
+++ src/main/java/com/example/readwatchlist/HelloController.java
@@ -3,35 +3,23 @@ package com.example.readwatchlist;
 import com.example.readwatchlist.domain.Book;
 import com.example.readwatchlist.domain.Movie;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.PrintWriter;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.text.Font;
-import javafx.geometry.Insets;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.TabPane;
 import javafx.scene.control.Tab;
-import java.util.Scanner;
-
-import java.io.File;
-import java.io.PrintWriter;
 
 public class HelloController {
 
