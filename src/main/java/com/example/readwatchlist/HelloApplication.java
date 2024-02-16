2eaf2c7afcea135a5a7f03534e42f4e7f7e929aa
David <11443294+davidabrowning@users.noreply.github.com>
2024-02-16 14:02:55 -0500

    Removed unused import statements.

--- src/main/java/com/example/readwatchlist/HelloApplication.java
+++ src/main/java/com/example/readwatchlist/HelloApplication.java
@@ -1,27 +1,10 @@
 package com.example.readwatchlist;
 
-import com.example.readwatchlist.domain.Book;
-
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.text.Font;
-import javafx.geometry.Insets;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.Tab;
 
